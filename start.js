3e89c99f08ab8ef1fcbd3bbb00760a659fee487b
Marven Wilson Donque <marvenwilsons@gmail.com>
2018-11-28 13:15:41 -0500

    start refactor touch

---
+++ start.js
@@ -0,0 +1,11 @@
+const fileSystem = require('./lib/lib_dqfs.exts')
+const fs = new fileSystem
+
+// try{
+//     fs
+//         .cd('./lib/foo')
+//         .mkdir(['baz', 'ben'])
+//         // .touch(['rm.js'],['','',''])
+// }catch(e){
+//     console.log(e)
+// }

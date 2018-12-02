775c65b15c951534655c382a099353143e688d9f
Marven Wilson Donque <marvenwilsons@gmail.com>
2018-12-02 14:21:20 -0500

    refactor mkdir

--- start.js
+++ start.js
@@ -2,11 +2,11 @@ const fileSystem = require('./lib/lib_dqfs.exts')
 const nodefs = require('fs')
 const fs = new fileSystem
 
-// try{
-//     fs
-//         .cd('./lib/foo')
-//         .mkdir(['baz', 'ben'])
-//         // .touch(['rm.js'],['','',''])
-// }catch(e){
-//     console.log(e)
-// }
+try{
+    const x = fs
+    .cd('./lib/foo')
+    .ls()
+    console.log(x)
+}catch(e){
+    console.log(e)
+}
